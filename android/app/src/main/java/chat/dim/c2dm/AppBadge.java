29860868acd8c508e474b1b4b30b2849bec9058d
Moky <albert.moky@gmail.com>
2025-04-21 23:59:01 +0800

    Tarsier v1.4.1 released

--- android/app/src/main/java/chat/dim/c2dm/AppBadge.java
+++ android/app/src/main/java/chat/dim/c2dm/AppBadge.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 
 import com.google.firebase.messaging.RemoteMessage;
 
+import java.util.Date;
 import java.util.Map;
 
 import chat.dim.type.Converter;
@@ -16,23 +17,37 @@ class AppBadge {
 
    static void handleIntent(Intent intent, Context context) {
       try {
-         // fetch badge count from message data
-         int count = AppBadge.getCount(intent);
-         if (count >= 0) {
-            AppBadge.applyCount(count, context);
+         Bundle extra = intent.getExtras();
+         if (extra == null) {
+            return;
+         }
+         RemoteMessage message = new RemoteMessage(extra);
+         Map<String, String> data = message.getData();
+         // get message time
+         Date when = Converter.getDateTime(data.get("time"), null);
+         Date last = lastTime;
+         // get message count
+         int count = getCount(message.getNotification(), data);
+         if (count < 0) {
+            Log.warning("ignore message: " + data);
+         } else if (when == null) {
+            Log.warning("abnormal message: " + data);
+            applyCount(count, context);
+         } else if (last == null || last.before(when)) {
+            Log.info("update message time: " + when);
+            lastTime = when;
+            applyCount(count, context);
+         } else {
+            Log.warning("ignore expired message: " + data);
          }
       } catch (Exception e) {
          Log.error("failed to fetch badge count: " + e);
       }
    }
 
-   static int getCount(Intent intent) {
-      Bundle extra = intent.getExtras();
-      if (extra == null) {
-         return -1;
-      }
-      RemoteMessage message = new RemoteMessage(extra);
-      RemoteMessage.Notification notification = message.getNotification();
+   private static Date lastTime;
+
+   private static int getCount(RemoteMessage.Notification notification, Map<String, String> data) {
       // 1. get from notification
       if (notification != null) {
          Integer count = notification.getNotificationCount();
@@ -41,7 +56,6 @@ class AppBadge {
          }
       }
       // 2. get from data
-      Map<String, String> data = message.getData();
       Object value = data.get("badge_count");
       if (value == null) {
          value = data.get("badge");
@@ -49,7 +63,7 @@ class AppBadge {
       return Converter.getInt(value, -1);
    }
 
-   static void applyCount(int count, Context context) {
+   private static void applyCount(int count, Context context) {
       if (count < 0) {
          Log.error("badge count error: " + count);
          return;
